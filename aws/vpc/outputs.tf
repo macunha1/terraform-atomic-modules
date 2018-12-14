50f4cba5871b7057e2ef6bcace53b20aa933b462
Matheus Cunha <matheuz.a@gmail.com>
2018-12-14 17:48:11 -0200

    feat(vpc): Adding more outputs for VPC module

--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -1,19 +1,34 @@
 output "id" {
-  value = "${aws_vpc.default.id}"
+  description = "AWS VPC Id"
+  value       = "${aws_vpc.default.id}"
 }
 
 output "private_subnets" {
-  value = "${aws_subnet.private.*.id}"
+  description = "List of private subnets"
+  value       = "${aws_subnet.private.*.id}"
+}
+
+output "private_subnets_azs" {
+  description = "List of availability zones being used by private subnets"
+  value       = "${aws_subnet.private.*.availability_zone}"
 }
 
 output "public_subnets" {
-  value = "${aws_subnet.public.*.id}"
+  description = "List of public subnets"
+  value       = "${aws_subnet.public.*.id}"
+}
+
+output "public_subnet_azs" {
+  description = "List of availability zones being used by public subnets"
+  value       = "${aws_subnet.public.*.availability_zone}"
 }
 
 output "environment" {
+  description = "Environment name for this module"
   value = "${var.env}"
 }
 
 output "project_name" {
-  value = "${var.project_name}"
+  description = "Project name that this VPC serves"
+  value       = "${var.project_name}"
 }
